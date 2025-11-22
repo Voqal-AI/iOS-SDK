c37d92eb6fa69d5b145c58dab745c7bfa4084665
Mohamed Matloub <Mohamed.a.matloub@gmail.com>
2025-11-22 14:23:41 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "VoqalSDK",
             url: "https://voqal-sdk-ios.s3.me-south-1.amazonaws.com/VoqalSDK-0.1.2.zip",
-            checksum: "dffad1e17c82f52e97586f5e1797c57630452064ed8261e863e91c7f5ecf5a0c"
+            checksum: "8ad740b8903f8b10a8ae454a7e38f7dca7d003af966ee465fd87ecf795b32441"
         ),
     ]
 )
